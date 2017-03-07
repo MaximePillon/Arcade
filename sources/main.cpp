3b0537dfbd82d3e4000a804029461a75f1a4975f
Hugo SOSZYNSKI <hugo.soszynski@epitech.eu>
2017-03-07 18:53:18 +0100

    Added files to gitignore and added header to main

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,5 +1,15 @@
+/*
+** main.cpp for cpp_arcade in /home/soszyn_h/rendu/CPP/cpp_arcade/sources/main.cpp
+**
+** Made by Hugo SOSZYNSKI
+** Login   <hugo.soszynski@epitech.eu>
+**
+** Started on  Tue Mar 07 17:52:18 2017 Hugo SOSZYNSKI
+** Last update Tue Mar 07 17:52:18 2017 Hugo SOSZYNSKI
+*/
+
 int		main(int argc, const char *argv[])
 {
-  //ceci est un main mdr lol
+  // ceci est un main mdr lol
   return 0;
 }
