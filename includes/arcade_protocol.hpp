bc4c70193bb8d6c81ace1f8b8bee89f07bb8dcf1
Hugo SOSZYNSKI <hugo.soszynski@epitech.eu>
2017-04-08 00:20:39 +0200

    Added actions to the protocol

--- includes/arcade_protocol.hpp
+++ includes/arcade_protocol.hpp
@@ -1,74 +1,73 @@
-/*
-** arcade_protocol.hpp for cpp_arcade in /home/soszyn_h/rendu/CPP/cpp_arcade/includes/arcade_protocol.hpp
-**
-** Made by Hugo SOSZYNSKI
-** Login   <hugo.soszynski@epitech.eu>
-**
-** Started on  Tue Apr 04 13:59:27 2017 Hugo SOSZYNSKI
-** Last update Tue Apr 04 13:59:27 2017 Hugo SOSZYNSKI
-*/
+// Kind of Advanced Language Assistant Laboratory 2006-2042
+// Epitech 1999-2042
+// Jason Brillante brilla_a brilla_b
+// Have you played Atari today?
+//
+// WELCOME TO THE ARCADE
+// ENJOY OR DIE
 
-#ifndef ARCADE_PROTOCOL_HPP
-# define ARCADE_PROTOCOL_HPP
+#ifndef			__ARCADE_PROTOCOL_HPP__
+# define		__ARCADE_PROTOCOL_HPP__
+# include		<stdint.h>
 
-# include <stdint.h>
-
-namespace arcade
+namespace		arcade
 {
-  /// \enum CommandType
-  enum class CommandType : uint16_t
+  enum class		CommandType : uint16_t
   {
-    WHERE_AM_I = 0, //!< RETURN A WHERE AM I STRUCTURE
-    GET_MAP = 1, //!< RETURN A GETMAP STRUCTURE
-    GO_UP = 2, //!< MOVE THE CHARACTER UP
-    GO_DOWN = 3, //!< MOVE THE CHARACTER DOWN
-    GO_LEFT = 4, //!< MOVE THE CHARACTER LEFT
-    GO_RIGHT = 5, //!< MOVE THE CHARACTER RIGHT
-    GO_FORWARD = 6, //!< USELESS
-    SHOOT = 7, //!< SHOT (FOR SOLAR FOX AND CENTIPEDE)
-    ILLEGAL = 8, //!< THE INSTRUCTION WAS ILLEGAL
-    PLAY = 9 //!< PLAY A ROUND
+    WHERE_AM_I		= 0,		// RETURN A WHERE AM I STRUCTURE
+    GET_MAP		= 1,		// RETURN A GETMAP STRUCTURE
+    GO_UP		= 2,		// MOVE THE CHARACTER UP
+    GO_DOWN		= 3,		// MOVE THE CHARACTER DOWN
+    GO_LEFT		= 4,		// MOVE THE CHARACTER LEFT
+    GO_RIGHT		= 5,		// MOVE THE CHARACTER RIGHT
+    GO_FORWARD		= 6,		// MOVE THE CHARACTER FORWARD (FOR SNAKE)
+    SHOOT		= 7,		// SHOOT (FOR SOLAR FOX AND CENTIPEDE)
+    ILLEGAL		= 8,		// THE INSTRUCTION WAS ILLEGAL
+    PLAY		= 9,		// PLAY A ROUND
+    CLOSE		= 10,		// QUIT THE GAME
+    PREVIOUS_GL		= 11,		// SWITCH TO THE PREVIOUS GRAPHIC LIBRARY
+    NEXT_GL		= 12,		// SWITCH TO THE NEXT GRAPHIC LIBRARY
+    PREVIOUS_GAME	= 13, 		// SWITCH TO THE PREVIOUS GAME
+    NEXT_GAME		= 14,		// SWITCH TO THE NEXT GAME
+    RESTART		= 15,		// RESTART THE GAME
+    MENU		= 16		// GO TO THE MENU
   };
 
-  /// \enum TileType
-  enum class TileType : uint16_t
+  enum class		TileType : uint16_t
   {
-    EMPTY = 0, //!< TILE WHERE THE CHARACTER CAN GO
-    BLOCK = 1, //!< TILE WHERE THE CHARACTER CANNOT GO
-    OBSTACLE = 2, //!< FOR CENTIPEDE
-    EVIL_DUDE = 3, //!< EVIL DUDE
-    EVIL_SHOOT = 4, //!< EVIL SHOOT
-    MY_SHOOT = 5, //!< YOUR OWN SHOOT
-    POWERUP = 6, //!< POWERUP
-    OTHER = 7 //!< ANYTHING THAT WILL BE IGNORED BY THE KOALINETTE
+    EMPTY		= 0,		// TILE WHERE THE CHARACTER CAN GO
+    BLOCK		= 1,		// TILE WHERE THE CHARACTER CANNOT GO
+    OBSTACLE		= 2,		// FOR CENTIPEDE
+    EVIL_DUDE		= 3,		// EVIL DUDE
+    EVIL_SHOOT		= 4,		// EVIL SHOOT
+    MY_SHOOT		= 5,
+    POWERUP		= 6,		// POWERUP
+    OTHER		= 7		// ANYTHING THAT WILL BE IGNORED BY THE KOALINETTE
   };
 
-  /// \struct GetMap
-  /// \brief The format is width, height, and width * height * sizeof (TileType) quantity of TileType
-  struct GetMap
+  /// The format is width, height, and width * height * sizeof(TileType) quantity of TileType
+  struct		GetMap
   {
-    CommandType type;
-    uint16_t width;
-    uint16_t height;
-    TileType tile[0]; //!< SPECIFY AN ADDITIONAL SIZE WHILE ALLOCATING
-  } __attribute__ ((packed));
+    CommandType		type;
+    uint16_t		width;
+    uint16_t		height;
+    TileType		tile[0];
+  } __attribute__((packed));
 
-
-  /// \struct Position
-  /// \brief The format is length, length * Position quantity of TileType
-  struct Position
+  /// The format is length, length * Position quantity of TileType
+  struct		Position
   {
-    uint16_t x;
-    uint16_t y;
-  } __attribute__ ((packed));
+    uint16_t		x;
+    uint16_t		y;
+  } __attribute__((packed));
 
-  /// \struct WhereAmI
-  struct WhereAmI
+  struct		WhereAmI
   {
-    CommandType type;
-    uint16_t lenght;
-    Position position[0]; //!< SPECIFY AN ADDITIONAL SIZE WHILE ALLOCATING
-  } __attribute__ ((packed));
+    CommandType		type;
+    uint16_t		lenght;
+    Position		position[0];
+  } __attribute__((packed));
 }
 
-#endif // !ARCADE_PROTOCOL_HPP
+#endif	//		__ARCADE_PROTOCOL_HPP__
+
